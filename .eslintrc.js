65d4637f2aafb459832511176a3497888b6376c6
earlgrey02 <san06036@naver.com>
2024-09-14 16:37:19 +0900

    Add: ESLint 관련 의존성 추가 및 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
-  extends: ['@react-native']
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked'
+      ],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      rules: {
+        '@typescript-eslint/consistent-type-exports': 'error',
+        '@typescript-eslint/consistent-type-imports': 'error',
+        '@typescript-eslint/no-misused-promises': 'off'
+      }
+    }
+  ],
+  extends: ['@react-native', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react-native/no-inline-styles': 'off'
+  }
 }
