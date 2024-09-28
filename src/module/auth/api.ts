43c3623ade92ab5aa69ef738eb725ba2cf469a3f
earlgrey02 <san06036@naver.com>
2024-09-28 21:23:40 +0900

    Feat: 회원가입 API 연동

--- src/module/auth/api.ts
+++ src/module/auth/api.ts
@@ -1,9 +1,11 @@
-import type { RefreshRequest, SignInRequest } from './dto/request'
-import type { RefreshResponse, SignInResponse } from './dto/response'
+import type { RefreshRequest, SignInRequest, SignUpRequest } from './dto/request'
+import type { RefreshResponse, SignInResponse, SignUpResponse } from './dto/response'
 import { POST } from '../../lib/axios/method.ts'
 
 const signIn = async (request: SignInRequest) => POST<SignInResponse>('/auth/sign-in', request)
 
+const signUp = async (request: SignUpRequest) => POST<SignUpResponse>('/auth/sign-up', request)
+
 const refresh = async (request: RefreshRequest) => POST<RefreshResponse>('/auth/refresh', request)
 
-export { signIn, refresh }
+export { signIn, signUp, refresh }
