5dbda9f6028a430fbbb80e943aff9d9077fed54d
earlgrey02 <san06036@naver.com>
2024-10-05 20:52:15 +0900

    Refactor: 불필요한 async 키워드 삭제

--- src/module/auth/api.ts
+++ src/module/auth/api.ts
@@ -2,10 +2,10 @@ import type { RefreshRequest, SignInRequest, SignUpRequest } from './dto/request
 import type { RefreshResponse, SignInResponse, SignUpResponse } from './dto/response'
 import { POST } from '../../lib/axios/method.ts'
 
-const signIn = async (request: SignInRequest) => POST<SignInResponse>('/auth/sign-in', request)
+const signIn = (request: SignInRequest) => POST<SignInResponse>('/auth/sign-in', request)
 
-const signUp = async (request: SignUpRequest) => POST<SignUpResponse>('/auth/sign-up', request)
+const signUp = (request: SignUpRequest) => POST<SignUpResponse>('/auth/sign-up', request)
 
-const refresh = async (request: RefreshRequest) => POST<RefreshResponse>('/auth/refresh', request)
+const refresh = (request: RefreshRequest) => POST<RefreshResponse>('/auth/refresh', request)
 
 export { signIn, signUp, refresh }
--- src/module/user/api.ts
+++ src/module/user/api.ts
@@ -1,6 +1,6 @@
 import type { UserResponse } from './dto/response'
 import { GET } from '../../lib/axios/method.ts'
 
-const getMyUser = async () => GET<UserResponse>('/user/my')
+const getMyUser = () => GET<UserResponse>('/user/my')
 
 export { getMyUser }
