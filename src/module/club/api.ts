05f70cbfe918ddfe98265986bf42a6f63cc2be55
earlgrey02 <san06036@naver.com>
2024-10-10 11:32:11 +0900

    Feat: 동아리 리스트 조회 API 연동

--- src/module/club/api.ts
+++ src/module/club/api.ts
@@ -1,6 +1,14 @@
 import { GET } from '../../lib/axios/method.ts'
 import type { ClubResponse } from './dto/response'
+import type { SearchClubPageRequest } from './dto/request'
 
 const getClubsByUserId = (userId: string) => GET<ClubResponse[]>(`/club/${userId}`)
 
-export { getClubsByUserId }
+const searchClubPage = (request: SearchClubPageRequest, size: number, lastId: string) =>
+  GET<ClubResponse[]>('/club/search', {
+    ...request,
+    size,
+    ...(lastId && { lastId })
+  })
+
+export { getClubsByUserId, searchClubPage }
