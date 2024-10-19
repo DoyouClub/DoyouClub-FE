fdcaff8442a265e63c3197ca114f1cc2044140eb
earlgrey02 <san06036@naver.com>
2024-10-20 00:06:39 +0900

    Fix: ClubListScreen에서 무한 스크롤이 작동하지 않던 문제 해결

--- src/screen/club/ClubListScreen.tsx
+++ src/screen/club/ClubListScreen.tsx
@@ -10,6 +10,10 @@ import { useInfiniteQuery } from 'react-query'
 import { searchClubPage } from '../../module/club/api.ts'
 import type { SearchClubPageRequest } from '../../module/club/dto/request'
 import { Activity, Tag } from '../../module/club/dto/enum.ts'
+import TouchableScale from '../../components/common/TouchableScale.tsx'
+import type { NavigationProp } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
+import type { NavigatorParamList } from '../../navigation/navigation'
 
 const pageSize = 5
 
@@ -26,8 +30,14 @@ const filters: Record<keyof Filter, Record<string, string>> = {
 }
 
 const ClubListScreen = () => {
+  const navigation = useNavigation<NavigationProp<NavigatorParamList>>()
   const [request, setRequest] = useState<SearchClubPageRequest>({ name: '' })
-  const { data: infiniteClub } = useInfiniteQuery(
+  const {
+    data: infiniteClub,
+    hasNextPage,
+    isFetching,
+    fetchNextPage
+  } = useInfiniteQuery(
     ['searchClubPage', request],
     ({ pageParam = undefined }) => searchClubPage(request, pageSize, pageParam as string),
     {
@@ -45,6 +55,10 @@ const ClubListScreen = () => {
     setFilterModal(true)
     setCurrentFilter(filter)
   }, [])
+
+  const onEndReachedHandler = useCallback(async () => {
+    if (hasNextPage && !isFetching) await fetchNextPage()
+  }, [fetchNextPage, hasNextPage, isFetching])
 
   return (
     <SafeAreaView
@@ -70,11 +84,17 @@ const ClubListScreen = () => {
         </View>
         <FlatList
           data={infiniteClub?.pages.flat()}
-          renderItem={({ item }) => <ClubItem club={item} />}
+          renderItem={({ item }) => (
+            <TouchableScale activeScale={0.98} onPress={() => navigation.navigate('clubInfo', { club: item })}>
+              <ClubItem club={item} />
+            </TouchableScale>
+          )}
           contentContainerStyle={{
             paddingTop: 5,
             paddingBottom: 10
           }}
+          onEndReached={onEndReachedHandler}
+          onEndReachedThreshold={0.4}
           listEmptyMessage="검색된 동아리가 없습니다."
         />
       </View>
