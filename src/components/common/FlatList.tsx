4fd07f49b5bc2b1e0c4578256edc87d1fdc0541b
earlgrey02 <san06036@naver.com>
2024-10-20 00:14:49 +0900

    Feat: FlatList 커스텀 컴포넌트에 새로고침 기능 구현

--- src/components/common/FlatList.tsx
+++ src/components/common/FlatList.tsx
@@ -1,20 +1,23 @@
 import type { FlatListProps } from 'react-native'
-import { FlatList as RNFlatList } from 'react-native'
+import { FlatList as RNFlatList, RefreshControl } from 'react-native'
 import { AnimatedView } from '../../lib/reanimated/animated.ts'
 import { FadeIn, FadeOut } from 'react-native-reanimated'
 import Text from './Text.tsx'
+import { useState } from 'react'
 
-type Props<T> = { listEmptyMessage?: string } & FlatListProps<T>
+type Props<T> = { listEmptyMessage?: string; useRefresh?: boolean; onRefresh?: () => void } & FlatListProps<T>
 
-const FlatList = <T,>({ listEmptyMessage, ...props }: Props<T>) => {
+const FlatList = <T,>({ listEmptyMessage, useRefresh = false, onRefresh, ...props }: Props<T>) => {
+  const [refreshing] = useState(false)
+
   return (
     <RNFlatList
       style={[{ height: '100%' }, props.style]}
       showsVerticalScrollIndicator={false}
       keyboardDismissMode="on-drag"
       contentContainerStyle={[{ gap: 10 }, props.contentContainerStyle]}
-      {...(listEmptyMessage && {
-        ListEmptyComponent: (
+      ListEmptyComponent={
+        listEmptyMessage ? (
           <AnimatedView
             style={{
               justifyContent: 'center',
@@ -31,8 +34,11 @@ const FlatList = <T,>({ listEmptyMessage, ...props }: Props<T>) => {
               {listEmptyMessage}
             </Text>
           </AnimatedView>
+        ) : (
+          <></>
         )
-      })}
+      }
+      refreshControl={useRefresh ? <RefreshControl refreshing={refreshing} onRefresh={onRefresh} /> : <></>}
       {...props}
     />
   )
